08ed19353e3bf92a8177d2679ac2b2fbd60922ed
Artoiss <aarttu@windowslive.com>
2019-10-20 12:30:04 +0300

    Updates

---
+++ precache-manifest.e6cb000c81f5ad3cf7155a3576a91516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee805c90846db8318b9ad542bfdc5a1",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "27bc1fe359def1923425",
+    "url": "/myapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0e86c4120b6cbdcf3780",
+    "url": "/myapp/static/js/2.16664321.chunk.js"
+  },
+  {
+    "revision": "27bc1fe359def1923425",
+    "url": "/myapp/static/js/main.9845acc9.chunk.js"
+  },
+  {
+    "revision": "27343e7b7cdb70592f08",
+    "url": "/myapp/static/js/runtime-main.f1bd5847.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/myapp/static/media/logo.25bf045c.svg"
+  }
+]);
