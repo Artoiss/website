39a871d8a3a3345d42aeab6f5c8727a866ba3643
Artoiss <aarttu@windowslive.com>
2019-10-20 16:42:18 +0300

    Dog button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useState } from 'react'
 import hoppu from './hoppu.jpg';
+import hubert from './hubert.jpg';
 import './App.css';
 
+
 function App() {
+  const dogs = [hoppu, hubert]
+  const[selected, setSelected] = useState(Math.floor(Math.random() * dogs.length))
+
+  const handle = (props) => {
+    setSelected(Math.abs(props - 1))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={hoppu} className="App-logo" alt="logo" />
         <p>
-          Hoppu Development in progress.
+          Click button to change dog.
+        </p>
+        <button type="button" onClick= {() => handle(selected)}>Dog button</button>
+        <img src={dogs[selected]} className="App-logo" alt="logo" />
+        <p>
+          Hoppu Development in maintenance.
         </p>
         <a
           className="App-link"
