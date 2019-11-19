c65b9e8aa68e8a1403a8604797adabe3a70d67fc
Artoiss <aarttu@windowslive.com>
2019-11-19 21:46:19 +0200

    hoppulogo

--- src/Nav.js
+++ src/Nav.js
@@ -11,7 +11,7 @@ import {BrowserRouter as Router, Route, Link,
           <div className="navbar-brand">
             <Link to="/" className="navbar-brand">
               <p>
-                <img src={require("./dogs/hoppu.png")} />
+                <img src={require("./dogs/hoppu2.png")} />
               </p>
             </Link>
 
