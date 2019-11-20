ab90b778846e365f3e06362a5005ddf3a922528e
Artoiss <aarttu@windowslive.com>
2019-11-20 20:40:15 +0200

    animation

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ const Home=() => {
   return (
     <div className="Home">
       <header className="Home-header">
-        <p> Hoppu Development under maintenance </p>
+        <img src={require("./dogs/hubert2.png")} class="rotate2" width="100" height="120" />
         <img src={require("./dogs/hubert2.png")} class="rotate" width="100" height="120" />
       </header>
       <footer className="Home-footer">
